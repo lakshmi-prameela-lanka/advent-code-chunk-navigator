c01300157cd8b233fe5f947bda5f3a17d84c00a0
Lakshmi Prameela Lanka <lakshmi.prameela.lanka@gmail.com>
2022-06-11 09:58:59 +0200

    initialize input line and read them

--- src/main/java/com/chunk/navigation/ChunkNavigator.java
+++ src/main/java/com/chunk/navigation/ChunkNavigator.java
@@ -1,8 +1,27 @@
 package com.chunk.navigation;
 
+import java.util.List;
+
 public class ChunkNavigator {
 
-    public static void  main(String args[]){
-        System.out.println("hello world");
+    // initialize the input with given input lines
+    static List<String> allLines = List.of("[({(<(())[]>[[{[]{<()<>>",
+            "[(()[<>])]({[<{<<[]>>(",
+            "{([(<{}[<>[]}>{[]{[(<()>",
+            "(((({<>}<{<{<>}{[]{[]{}",
+            "[[<[([]))<([[{}[[()]]]",
+            "[{[{({}]{}}([{[{{{}}([]",
+            "{<[[]]>}<{[{[{[]{()[[[]",
+            "[<(<(<(<{}))><([]([]()",
+            "<{([([[(<>()){}]>(<<{{",
+            "<{([{{}}[<[[[<>{}]]]>[]]");
+
+    static List<Character> openingChars = List.of('(','[','{','<');
+    static List<Character> closingChars = List.of(')',']','}','>');
+
+    public static void main(String[] args) {
+       for(String chunk: allLines){
+           System.out.println(chunk);
+       }
     }
 }
