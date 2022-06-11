a877e96336d68c5cd16b2afb8be8b3f957644801
Lakshmi Prameela Lanka <lakshmi.prameela.lanka@gmail.com>
2022-06-11 10:19:10 +0200

    split string to chars and traverse char array

--- src/main/java/com/chunk/navigation/ChunkNavigator.java
+++ src/main/java/com/chunk/navigation/ChunkNavigator.java
@@ -1,6 +1,8 @@
 package com.chunk.navigation;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 public class ChunkNavigator {
 
@@ -19,9 +21,28 @@ public class ChunkNavigator {
     static List<Character> openingChars = List.of('(','[','{','<');
     static List<Character> closingChars = List.of(')',']','}','>');
 
+    static Map<Character, Character> legalChunkCharMap = Map.of('(',')','[',']','{','}','<','>');
+
     public static void main(String[] args) {
        for(String chunk: allLines){
            System.out.println(chunk);
        }
+    }
+
+    public static int getErrorScoreOfaLine(String line){
+        int errorScore = 0;
+       Stack<Character> traversedChars  = new Stack<>();
+        for(Character currentChar: line.toCharArray()){
+            System.out.print(currentChar);
+            if(isOpeningChar(currentChar)){
+                traversedChars.push(currentChar);
+            }
+        }
+        System.out.println(traversedChars);
+        return errorScore;
+    }
+
+    public static boolean isOpeningChar(Character character){
+        return openingChars.contains(character);
     }
 }
--- src/test/java/com/chunk/navigation/ChunkNavigatorTest.java
+++ src/test/java/com/chunk/navigation/ChunkNavigatorTest.java
@@ -1,4 +1,25 @@
 package com.chunk.navigation;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ChunkNavigatorTest {
+    @Test
+    void getErrorScoreOfaLine() {
+        // given
+        String line = "{([(<{}[<>[]}>{[]{[(<()>";
+        ChunkNavigator.getErrorScoreOfaLine(line);
+    }
+
+    @Test
+    void isOpeningCharTestTrue() {
+        assertTrue(ChunkNavigator.isOpeningChar('('));
+    }
+
+    @Test
+    void isOpeningCharTestFalse() {
+        assertFalse(ChunkNavigator.isOpeningChar(']'));
+    }
 }
